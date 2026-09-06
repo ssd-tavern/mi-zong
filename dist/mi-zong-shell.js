735772d5727d6c44a3c03f04a9c656b8efcd0ded
ssd-tavern <ssd-tavern@users.noreply.github.com>
2026-09-05 17:24:30 -0700

    v3.0.8 驿路前四站章头图改长安风

--- dist/mi-zong-shell.js
+++ dist/mi-zong-shell.js
@@ -4,7 +4,7 @@
   var SHELL_ID = "mz-shell-root";
   var SHELL_TOKEN = "mz_" + Math.random().toString(36).slice(2) + "_" + Date.now();
   var CARD_TITLE = "密宗模拟器";
-  var CDN_TAG = "3.0.7";
+  var CDN_TAG = "3.0.8";
   var FONT_PKG = "@fontsource/noto-serif-sc@5.3.0";
   var FONT_CSS = [400, 600].map((w) => "https://testingcf.jsdelivr.net/npm/" + FONT_PKG + "/" + w + ".css");
   var FONT_LINK_ID = "mz-font-";
